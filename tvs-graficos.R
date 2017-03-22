6b7f36b68f6b4f41fb6fd141eb538c5f1d80b044
Omar Gonzales <omagonza1@publicisgroupe.net>
2017-03-22 16:49:37 -0500

    modificaciones a los gráficos de marcas por rango de precio

--- tvs-graficos.R
+++ tvs-graficos.R
@@ -209,7 +209,7 @@ tvs.precios$ecommerce <- factor(tvs.precios$ecommerce, levels = c("linio",
 
 
 p <- plot_ly(tvs.precios, x = ~periodo, y = ~precio.actual, color = ~ecommerce, type = "box") %>%
-  layout(boxmode = "group", title = "Double Y Axis")
+  layout(boxmode = "group", title = "Distribución de los precios de TVs por Ecommerce")
 
 p
 
@@ -232,6 +232,29 @@ p
 
 tvs.ripley <- tvs[tvs$ecommerce == "ripley",]
 
+
+### falabella ###
+
+tvs.falabella <- tvs[tvs$ecommerce == "falabella",]
+
+
+### linio ###
+
+tvs.linio <- tvs[tvs$ecommerce == "linio",]
+
+
+
+#########################################################
+### MARCAS DE TV POR RANGO DE PRECIO - EN PORCENTAJES ###
+#########################################################
+
+
+
+
+### Ripley
+
+
+
 tvs.ripley.tvs.por.rango <- tvs.ripley  %>%
                             group_by(periodo, rango, marca) %>%
                             summarise(cantidad.marca = length(marca)) %>% 
@@ -245,6 +268,22 @@ tvs.ripley.tvs.por.rango <- tvs.ripley  %>%
 
 tvs.ripley.tvs.por.rango$periodo <- factor(tvs.ripley.tvs.por.rango$periodo, levels = c(2017,2016),
                                          ordered = T)
+
+
+
+
+
+
+
+tvs.ripley.tvs.por.rango$rango <- factor(tvs.ripley.tvs.por.rango$rango, levels = c("< S/.500",
+                                                      "S/.500 -\r\n S/.1500",
+                                                      "S/.1500 -\r\n S/.2500",
+                                                      "S/.2500 -\r\n S/.3500",
+                                                      "S/.3500 -\r\n S/.4500",
+                                                      "> S/.4,500"),
+                          ordered = T)
+
+
 
 
 
@@ -286,11 +325,198 @@ ggplot(tvs.ripley.tvs.por.rango, aes(x=rango, y= porcentaje_tvs ,fill=marca)) +
 
 
 
+### Falabella
+
+
+
+tvs.falabella.tvs.por.rango <- tvs.falabella  %>%
+  group_by(periodo, rango, marca) %>%
+  summarise(cantidad.marca = length(marca)) %>% 
+  #mutate(porcentaje = paste0(round(cantidad.marca/sum(cantidad.marca)*100,2),'%'))
+  mutate(porcentaje_tvs = round(cantidad.marca/sum(cantidad.marca),2))
+
+
+
+
+
+tvs.falabella.tvs.por.rango$periodo <- factor(tvs.falabella.tvs.por.rango$periodo, levels = c(2017,2016),
+                                              ordered = T)
+
+
+
+tvs.falabella.tvs.por.rango$rango <- factor(tvs.falabella.tvs.por.rango$rango, levels = c("< S/.500",
+                                                                                    "S/.500 -\r\n S/.1500",
+                                                                                    "S/.1500 -\r\n S/.2500",
+                                                                                    "S/.2500 -\r\n S/.3500",
+                                                                                    "S/.3500 -\r\n S/.4500",
+                                                                                    "> S/.4,500"),
+                                         ordered = T)
+
+
+
+tt5 <- "Sagafalabella \n % marcas de tvs por rango de precios"
+stt5 <- "\n"
+
+
+
+ggplot(tvs.falabella.tvs.por.rango, aes(x=rango, y= porcentaje_tvs ,fill=marca)) + 
+  geom_bar(stat = "identity", width = .7) +
+  facet_grid(~ periodo) +
+  theme_bw() +
+  #theme_ipsum_rc(grid = "Y") +
+  theme(axis.text.x = element_text(colour="grey20",size=12,hjust=.5,vjust=.5,face="plain"),
+        axis.text.y = element_text(colour="grey20",size=10,hjust=1,vjust=0,face="plain"),  
+        axis.title.x = element_text(colour="grey20",size=18,angle=0,hjust=.5,vjust=0,face="plain"),
+        axis.title.y = element_text(colour="grey20",size=16,angle=90,hjust=.5,vjust=.5,face="plain"),
+        plot.title = element_text(vjust=2, size = 24,face="bold"),
+        plot.subtitle = element_text(vjust=2, size = 16),
+        plot.caption = element_text(vjust=2, size = 16),
+        legend.position = "top",
+        legend.box = "horizontal",
+        legend.title=element_blank(),
+        legend.text=element_text(size=18),
+        strip.text = element_text(size = 22, hjust = 0.05, vjust = -.5, face = "bold")) +
+  scale_y_continuous(labels=percent) +
+  # scale_fill_manual(
+  #   values = c("hisense" = "#F39EF7","sony" = "#003366","panasonic" = "#FCB462",
+  #              "samsung" = "#7ec0ee", "lg" = "#A21420",
+  #              "aoc" = "#9DCC27", "sharp" = "#BEBBDA", "hyundai" = "#FCB442")) +
+  # labs(title = tt5, subtitle = stt5, caption = cptn,
+  #      x = "", y = "") +
+  labs(title = tt5, subtitle = stt5, caption = cptn,
+       x = "", y = "") 
+
+
+
+### Linio
+
+
+
+tvs.linio.tvs.por.rango <- tvs.linio  %>%
+                            group_by(periodo, rango, marca) %>%
+                            summarise(cantidad.marca = length(marca)) %>% 
+                            #mutate(porcentaje = paste0(round(cantidad.marca/sum(cantidad.marca)*100,2),'%'))
+                            mutate(porcentaje_tvs = cantidad.marca/sum(cantidad.marca))
+
+
+
+
+
+
+
+
+tvs.linio.tvs.por.rango$periodo <- factor(tvs.linio.tvs.por.rango$periodo, levels = c(2017,2016),
+                                              ordered = T)
+
+
+
+
+tvs.linio.tvs.por.rango$rango <- factor(tvs.linio.tvs.por.rango$rango, levels = c("< S/.500",
+                                                                                          "S/.500 -\r\n S/.1500",
+                                                                                          "S/.1500 -\r\n S/.2500",
+                                                                                          "S/.2500 -\r\n S/.3500",
+                                                                                          "S/.3500 -\r\n S/.4500",
+                                                                                          "> S/.4,500"),
+                                            ordered = T)
+
+
+
+
+tt5 <- "Linio \n % marcas de tvs por rango de precios"
+stt5 <- "\n"
+
+
+
+ggplot(tvs.linio.tvs.por.rango, aes(x=rango, y= porcentaje_tvs ,fill=marca)) + 
+  geom_bar(stat = "identity", width = .7) +
+  facet_grid(~ periodo) +
+  theme_bw() +
+  #theme_ipsum_rc(grid = "Y") +
+  theme(axis.text.x = element_text(colour="grey20",size=12,hjust=.5,vjust=.5,face="plain"),
+        axis.text.y = element_text(colour="grey20",size=10,hjust=1,vjust=0,face="plain"),  
+        axis.title.x = element_text(colour="grey20",size=18,angle=0,hjust=.5,vjust=0,face="plain"),
+        axis.title.y = element_text(colour="grey20",size=16,angle=90,hjust=.5,vjust=.5,face="plain"),
+        plot.title = element_text(vjust=2, size = 24,face="bold"),
+        plot.subtitle = element_text(vjust=2, size = 16),
+        plot.caption = element_text(vjust=2, size = 16),
+        legend.position = "top",
+        legend.box = "horizontal",
+        legend.title=element_blank(),
+        legend.text=element_text(size=18),
+        strip.text = element_text(size = 22, hjust = 0.05, vjust = -.5, face = "bold")) +
+  scale_y_continuous(labels=percent) +
+  # scale_fill_manual(
+  #   values = c("hisense" = "#F39EF7","sony" = "#003366","panasonic" = "#FCB462",
+  #              "samsung" = "#7ec0ee", "lg" = "#A21420",
+  #              "aoc" = "#9DCC27", "sharp" = "#BEBBDA", "hyundai" = "#FCB442")) +
+  # labs(title = tt5, subtitle = stt5, caption = cptn,
+  #      x = "", y = "") +
+  labs(title = tt5, subtitle = stt5, caption = cptn,
+       x = "", y = "") 
+
+
+
+
+
+# 
+# solo.linio <- plot_ly(tvs.linio.porcentajes, x = ~marca, y = ~dif.porcentual, color = ~marca, type = "box") %>%
+#   layout(boxmode = "group")
+# 
+# solo.linio
+
+
+
+
+tt8 <- "Linio - variación % descuentos según rango de precios"
+stt8 <- "\n"
+# 
+# 
+# 
+l <- ggplot(tvs.linio.porcentajes, aes(factor(marca), dif.porcentual))
+
+
+
+l + geom_boxplot(aes(colour = rango), outlier.colour = "red", outlier.shape = 1,
+                 outlier.size = 4) +
+  #facet_grid_paginate(periodo ~ rango, page = 1) +
+  #theme_ipsum_rc(grid = "Y") +
+  theme(axis.text.x = element_text(colour="grey20",size=12,hjust=.5,vjust=.5,face="plain"),
+        axis.text.y = element_text(colour="grey20",size=10,hjust=1,vjust=0,face="plain"),  
+        axis.title.x = element_text(colour="grey20",size=18,angle=0,hjust=.5,vjust=0,face="plain"),
+        axis.title.y = element_text(colour="grey20",size=16,angle=90,hjust=.5,vjust=.5,face="plain"),
+        plot.title = element_text(vjust=2, size = 24,face="bold"),
+        plot.subtitle = element_text(vjust=2, size = 16),
+        plot.caption = element_text(vjust=2, size = 16),
+        legend.position = "none",
+        legend.box = "horizontal",
+        legend.title=element_blank(),
+        legend.text=element_text(size=18),
+        strip.text = element_text(size = 22, hjust = 0, vjust = -.5, face = "bold")) +
+  scale_y_continuous(labels=comma) +
+  # scale_fill_manual(
+  #   values = c("hisense" = "#F39EF7","sony" = "#003366","panasonic" = "#FCB462",
+  #              "samsung" = "#7ec0ee", "lg" = "#A21420",
+  #              "aoc" = "#9DCC27", "sharp" = "#BEBBDA", "hyundai" = "#FCB442")) +
+  # labs(title = tt5, subtitle = stt5, caption = cptn,
+  #      x = "", y = "") +
+  labs(title = tt8, subtitle = stt8, caption = cptn,
+       x = "", y = "") 
+  #scale_color_ipsum() 
+
+
+
+
+
+
+
+#########
+
+
 
 
 
 precio.actual.ripley <- plot_ly(tvs.ripley, x = ~periodo, y = ~precio.actual, color = ~marca, type = "box") %>%
-  layout(boxmode = "group")
+  layout(boxmode = "group", title = "Ripley: distribución de los precios de TVs por marca")
 
 precio.actual.ripley
 
@@ -333,60 +559,6 @@ dif.porcentual.ripley <- plot_ly(tvs.ripley.dif.porcentual, x = ~periodo, y = ~d
 dif.porcentual.ripley
 
 
-
-
-### falabella ###
-
-
-
-tvs.falabella <- tvs[tvs$ecommerce == "falabella",]
-
-tvs.falabella.tvs.por.rango <- tvs.falabella  %>%
-                                group_by(periodo, rango, marca) %>%
-                                summarise(cantidad.marca = length(marca)) %>% 
-                                #mutate(porcentaje = paste0(round(cantidad.marca/sum(cantidad.marca)*100,2),'%'))
-                                mutate(porcentaje_tvs = round(cantidad.marca/sum(cantidad.marca),2))
-                              
-
-
-
-
-tvs.falabella.tvs.por.rango$periodo <- factor(tvs.falabella.tvs.por.rango$periodo, levels = c(2017,2016),
-                                            ordered = T)
-
-
-
-tt5 <- "Sagafalabella \n % marcas de tvs por rango de precios"
-stt5 <- "\n"
-
-
-
-ggplot(tvs.falabella.tvs.por.rango, aes(x=rango, y= porcentaje_tvs ,fill=marca)) + 
-  geom_bar(stat = "identity", width = .7) +
-  facet_grid(~ periodo) +
-  theme_bw() +
-  #theme_ipsum_rc(grid = "Y") +
-  theme(axis.text.x = element_text(colour="grey20",size=12,hjust=.5,vjust=.5,face="plain"),
-        axis.text.y = element_text(colour="grey20",size=10,hjust=1,vjust=0,face="plain"),  
-        axis.title.x = element_text(colour="grey20",size=18,angle=0,hjust=.5,vjust=0,face="plain"),
-        axis.title.y = element_text(colour="grey20",size=16,angle=90,hjust=.5,vjust=.5,face="plain"),
-        plot.title = element_text(vjust=2, size = 24,face="bold"),
-        plot.subtitle = element_text(vjust=2, size = 16),
-        plot.caption = element_text(vjust=2, size = 16),
-        legend.position = "top",
-        legend.box = "horizontal",
-        legend.title=element_blank(),
-        legend.text=element_text(size=18),
-        strip.text = element_text(size = 22, hjust = 0.05, vjust = -.5, face = "bold")) +
-  scale_y_continuous(labels=percent) +
-  # scale_fill_manual(
-  #   values = c("hisense" = "#F39EF7","sony" = "#003366","panasonic" = "#FCB462",
-  #              "samsung" = "#7ec0ee", "lg" = "#A21420",
-  #              "aoc" = "#9DCC27", "sharp" = "#BEBBDA", "hyundai" = "#FCB442")) +
-  # labs(title = tt5, subtitle = stt5, caption = cptn,
-  #      x = "", y = "") +
-  labs(title = tt5, subtitle = stt5, caption = cptn,
-       x = "", y = "") 
 
 
 
@@ -443,107 +615,6 @@ solo.saga
 
 
 
-
-tvs.linio <- tvs[tvs$ecommerce == "linio",]
-
-
-
-
-tvs.linio.porcentajes <- tvs.linio %>%
-  group_by(periodo, marca, precio.antes, precio.actual) %>%
-  mutate(dif.precios = precio.antes - precio.actual,
-         dif.porcentual = round(100*dif.precios/precio.antes,2))
-
-
-
-
-
-tvs.linio.porcentajes$periodo <- factor(tvs.linio.porcentajes$periodo, levels = c(2017,2016),
-                                        ordered = T)
-
-
-
-
-
-
-
-
-
-# scale_fill_manual(
-#   values = c("hisense" = "#EE6CF5","sony" = "#003366","panasonic" = "#FCB462",
-#              "samsung" = "#7ec0ee", "lg" = "#A21420", "altron" = "#E6981C",
-#              "aoc" = "#9DCC27", "sharp" = "#BEBBDA", "olitec" = "#3A76D6",
-#              "imaco" = "#00C18C", "nex" = "#DE8D00", "haier" = "#01BA38",
-#              "continental" = "#BD9C00", "daewoo" = "#968363", "king master" = "#EDED6F",
-#              "blackline" = "#5BD4BA", "hyundai" = "#01B3EF", "miray" = "#ED6F87",
-#              "royal" = "#5A9AED")) +
-
-
-
-
-
-
-
-
-
-
-
-
-tvs.linio.porcentajes$periodo <- factor(tvs.linio.porcentajes$periodo, levels = c(2017,2016),
-                                        ordered = T)
-
-# 
-# solo.linio <- plot_ly(tvs.linio.porcentajes, x = ~marca, y = ~dif.porcentual, color = ~marca, type = "box") %>%
-#   layout(boxmode = "group")
-# 
-# solo.linio
-
-
-
-
-tt8 <- "Linio - variación % descuentos según rango de precios"
-stt8 <- "\n"
-# 
-# 
-# 
-l <- ggplot(tvs.linio.porcentajes, aes(factor(marca), dif.porcentual))
-
-
-
-l + geom_boxplot(aes(colour = rango), outlier.colour = "red", outlier.shape = 1,
-                 outlier.size = 4) +
-  facet_grid_paginate(periodo ~ rango, page = 1) +
-  theme_ipsum_rc(grid = "Y") +
-  theme(axis.text.x = element_text(colour="grey20",size=12,hjust=.5,vjust=.5,face="plain"),
-        axis.text.y = element_text(colour="grey20",size=10,hjust=1,vjust=0,face="plain"),  
-        axis.title.x = element_text(colour="grey20",size=18,angle=0,hjust=.5,vjust=0,face="plain"),
-        axis.title.y = element_text(colour="grey20",size=16,angle=90,hjust=.5,vjust=.5,face="plain"),
-        plot.title = element_text(vjust=2, size = 24,face="bold"),
-        plot.subtitle = element_text(vjust=2, size = 16),
-        plot.caption = element_text(vjust=2, size = 16),
-        legend.position = "none",
-        legend.box = "horizontal",
-        legend.title=element_blank(),
-        legend.text=element_text(size=18),
-        strip.text = element_text(size = 22, hjust = 0, vjust = -.5, face = "bold")) +
-  scale_y_continuous(labels=comma) +
-  # scale_fill_manual(
-  #   values = c("hisense" = "#F39EF7","sony" = "#003366","panasonic" = "#FCB462",
-  #              "samsung" = "#7ec0ee", "lg" = "#A21420",
-  #              "aoc" = "#9DCC27", "sharp" = "#BEBBDA", "hyundai" = "#FCB442")) +
-  # labs(title = tt5, subtitle = stt5, caption = cptn,
-  #      x = "", y = "") +
-  labs(title = tt8, subtitle = stt8, caption = cptn,
-       x = "", y = "") +
-  scale_color_ipsum() 
-
-
-
-
-
-
-ggsave(file="l-tvs-porcentajes.jpg", width = 14, height = 8,
-       path = "D:\\RCoursera\\r-s-l\\graficos")
 
 
 
